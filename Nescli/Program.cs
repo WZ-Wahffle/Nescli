01dbc107b364a9b45ac7f2f3906bcc7aa8be986c
mvauderw <wz.wahffle@gmail.com>
2024-04-27 22:07:50 +0200

    Finally fixed the silly File reading, looks much nicer now

--- Nescli/Program.cs
+++ Nescli/Program.cs
@@ -1,52 +1,38 @@
-﻿using Nescli;
+﻿namespace Nescli;
 
-var f = File.OpenRead("/home/mvauderw/RiderProjects/Nescli/Nescli/smb1.nes");
-if (f == Stream.Null)
+internal static class Program
 {
-    Console.WriteLine("Invalid file");
-    return 1;
-}
-
-{
-    var b = new byte[4];
-    f.ReadExactly(b, 0, 4);
-    if (!b.SequenceEqual(new byte[] { 0x4e, 0x45, 0x53, 0x1a }))
+    public static void Main()
     {
-        Console.WriteLine("File header invalid");
-        return 1;
+        using var f = File.OpenRead("/home/mvauderw/RiderProjects/Nescli/Nescli/smb1.nes");
+        using BinaryReader reader = new(f);
+
+        // ensure file header is intact
+        if (!reader.ReadBytes(4).SequenceEqual(new byte[] { 0x4e, 0x45, 0x53, 0x1a }))
+        {
+            Console.WriteLine("File header invalid");
+            return;
+        }
+
+        var file = new Ines();
+        file.fileSize = f.Length;
+
+        file.asmSize = reader.ReadByte();
+        file.graphicsSize = reader.ReadByte();
+        file._headerUnused = reader.ReadBytes(10);
+
+        Console.WriteLine("File size: " + file.fileSize + "b");
+        Console.WriteLine("Program ROM size: " + file.asmSize * 16384 + "b");
+        Console.WriteLine("Graphics ROM size: " + file.graphicsSize * 8192 + "b");
+
+        var asmRom = reader.ReadBytes(16384 * file.asmSize);
+        var graphicsRom = reader.ReadBytes(8192 * file.graphicsSize);
+
+        var memoryController = new MemoryController();
+        memoryController.AddMemory(new Rom(asmRom), 0x8000, 0x10000);
+        var cpu = new Cpu(memoryController);
+
+        // temporary implementation to help find unimplemented opcodes
+        for (int i = 0; i < 100; i++) cpu.Run();
     }
 }
-
-var file = new Ines();
-file.fileSize = f.Length;
-
-{
-    var b = new byte[1];
-    f.ReadExactly(b, 0, 1);
-    file.asmSize = b[0];
-    f.ReadExactly(b, 0, 1);
-    file.graphicsSize = b[0];
-}
-{
-    var b = new byte[10];
-    f.ReadExactly(b, 0, 10);
-    file._headerUnused = (byte[])b.Clone();
-}
-Console.WriteLine("File size: " + file.fileSize + "b");
-Console.WriteLine("Program ROM size: " + file.asmSize * 16384 + "b");
-Console.WriteLine("Graphics ROM size: " + file.graphicsSize * 8192 + "b");
-
-var asmRom = new byte[16384 * file.asmSize];
-f.ReadExactly(asmRom, 0, 16384 * file.asmSize);
-var graphicsRom = new byte[8192 * file.graphicsSize];
-f.ReadExactly(graphicsRom, 0, 8192 * file.graphicsSize);
-f.Close();
-
-var memoryController = new MemoryController();
-memoryController.AddMemory(new Rom(asmRom), 0x8000, 0x10000);
-var cpu = new Cpu(memoryController);
-
-// temporary implementation to help find unimplemented opcodes
-for(int i = 0; i < 100; i++) cpu.Run();
-
-return 0;
